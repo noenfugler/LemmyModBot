51a196b12381855f6c237e2e56ef0b0d37f462ba
noenfugler <github@greenwolf.com.au>
2023-06-23 18:01:35 +1000

    updated bot filenames

--- credentials.py
+++
@@ -1,6 +0,0 @@
-# Rename this file to credentials.py and update the values below.
-username = "lemmy_bot"
-password = "9;=QH}%3-h~eZ#A"
-instance = "https://lemmy.world"
-community = "asklemmy@lemmy.world"
-alt_username = 'Bluetreefrog'

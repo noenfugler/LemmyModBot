1f43a7e852fc8783387851470e86b909165f4ded
noenfugler <github@greenwolf.com.au>
2023-06-23 17:24:10 +1000

    Created credentials_template.py

---
+++ credentials_template.py
@@ -0,0 +1,5 @@
+# Rename this file to credentials.py and update the values below.
+username = "username here"
+password = "password here"
+instance = "https://lemmy.ml"
+alt_username = 'username for real user who manages the bot.'

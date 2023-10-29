f6e2f2bacd0cfe502774349bc93b36779cbdf72f
Ben McLean <benmclean007@gmail.com>
2023-10-30 00:23:09 +1100

    Fix init module ignores

--- tests/processors/test_title_conformity_processor.py
+++ tests/processors/test_title_conformity_processor.py
@@ -12,7 +12,7 @@ class TestTitleConformityProcessor(unittest.TestCase):
         # Test when the content type is not POST_TITLE, no comment should be posted.
         processor = TitleConformityProcessor("your_regex_pattern", "Your message")
 
-        result = processor.execute(Content(
+        processor.execute(Content(
             "",
             "",
             "",
@@ -23,20 +23,20 @@ class TestTitleConformityProcessor(unittest.TestCase):
 
     def test_execute_comment(self):
         # Test when the content type is POST_TITLE and the pattern doesn't match, a comment should be posted.
-        processor = TitleConformityProcessor(".* \(\(", "Your message")
+        processor = TitleConformityProcessor(".* \\(\\(", "Your message")
 
         self.mock_content.type = ContentType.POST_TITLE
         self.mock_content.content = "Title that doesn't match the pattern"
-        result = processor.execute(self.mock_content, self.mock_handle)
+        processor.execute(self.mock_content, self.mock_handle)
 
         self.mock_handle.post_comment.assert_called_with("Your message")
 
     def test_execute_nothing(self):
         # Test when the content type is POST_TITLE and the pattern matches, no comment should be posted.
-        processor = TitleConformityProcessor(".* \(\(", "Your message")
+        processor = TitleConformityProcessor(".* \\(\\(", "Your message")
 
         self.mock_content.type = ContentType.POST_TITLE
         self.mock_content.content = "Title that matches the pattern (("
-        result = processor.execute(self.mock_content, self.mock_handle)
+        processor.execute(self.mock_content, self.mock_handle)
 
         self.mock_handle.post_comment.assert_not_called()

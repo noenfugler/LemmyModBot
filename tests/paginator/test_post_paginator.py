561deb3c779c08b7c00370085559e35e05d3f157
Ian Oxborrow <ian@Ians-MacBook-Pro-2.local>
2023-11-21 23:04:15 +1100

    added test to check if the final page is called correctly

--- tests/paginator/test_post_paginator.py
+++ tests/paginator/test_post_paginator.py
@@ -16,7 +16,7 @@ class TestPostPaginator(unittest.TestCase):
             response.json.return_value = json.loads(f.read())
             self.mock_lemmy.get_community.return_value = response
 
-    def test_get_page_response_called_correct_number_of_times(self):
+    def test_task_called_correct_number_of_times(self):
 
         mock_persistence = Mock()
         mock_persistence.get_current_page.return_value = 1
@@ -44,7 +44,33 @@ class TestPostPaginator(unittest.TestCase):
         assert mock_task.call_count == 10
 
     def test_correct_final_current_page(self):
-        pass
+        mock_persistence = Mock()
+        mock_persistence.get_current_page.return_value = 1
+        mock_persistence.set_current_page.method()
 
-    def test_task_called_correct_number_of_times(self):
-        pass
+        post_paginator = PostPaginator(
+            lemmy=self.mock_lemmy,
+            community_name="196",
+            monitor_persistence=mock_persistence
+        )
+
+        post_ten = json.loads(Path('tests/fixtures/get_posts_response_ok.json').read_text())
+        post_empty = json.loads(Path('tests/fixtures/get_posts_response_empty.json').read_text())
+
+        response = Mock()
+        response.json.side_effect = [post_ten, post_empty]
+
+        self.mock_lemmy.get_posts.return_value = response
+
+        mock_task = Mock()
+        mock_task.method()
+
+        post_paginator.paginate(mock_task, 1, 10)
+
+        self.mock_lemmy.get_posts.assert_called_with(
+            community_id=70513,
+            page=2,
+            limit=10,
+            sort="Old"
+        )
+

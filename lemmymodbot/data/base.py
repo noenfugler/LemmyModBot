8fb3a062d8cd6d1640563e1cb739c9c54506de67
Ian Oxborrow <ian@Ians-MacBook-Pro-2.local>
2023-11-21 19:54:57 +1100

    added database files

---
+++ lemmymodbot/data/base.py
@@ -0,0 +1,38 @@
+from contextlib import contextmanager
+from typing import Optional, Generator, ContextManager
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, VARCHAR, TEXT, ForeignKey, create_engine, Engine
+from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy.orm import Mapped, mapped_column, relationship, sessionmaker, Session
+
+Base = declarative_base()
+metadata = Base.metadata
+
+engine: Engine = create_engine('sqlite:///data/database.db', echo=False)
+session_maker = sessionmaker(bind=engine)
+
+
+@contextmanager
+def session_scope() -> Session:
+    """Provide a transactional scope around a series of operations."""
+    session = session_maker()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+class CurrentPage(Base):
+    __tablename__ = 'current_page'
+
+    community_name = Column(VARCHAR(30), primary_key=True)
+    post_page = Column(INTEGER)
+    comment_page = Column(INTEGER)
+
+
+Base.metadata.create_all(engine)
